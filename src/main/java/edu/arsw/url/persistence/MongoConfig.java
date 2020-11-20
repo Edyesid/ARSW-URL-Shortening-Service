a99cfa1f0a959ec164e4e37b67e0935ceb0cde04
Edyesid <edyesid2000@gmail.com>
2020-11-20 18:56:10 -0500

    6

--- src/main/java/edu/arsw/url/persistence/MongoConfig.java
+++ src/main/java/edu/arsw/url/persistence/MongoConfig.java
@@ -1,4 +1,4 @@
-package edu.arsw.url.persistence;
+/*package edu.arsw.url.persistence;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import org.springframework.beans.factory.annotation.Value;
@@ -7,11 +7,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 @Configuration
 public class MongoConfig {
-    /*
+
     @Value("${spring.data.mongodb.uri}")
     private String uri;
     @Value("${spring.data.mongodb.database}")
-    private String dbname;*/
+    private String dbname;
     @Bean
     public MongoClient mongoClient() {
         return MongoClients.create("mongodb+srv://edwin_arsw:edwin123@cluster0.s2ygl.mongodb.net/UrlShortingDB?retryWrites=true&w=majority");
@@ -21,3 +21,4 @@ public class MongoConfig {
         return new MongoTemplate(mongoClient(), "UrlShortingDB");
     }
 }
+*/
