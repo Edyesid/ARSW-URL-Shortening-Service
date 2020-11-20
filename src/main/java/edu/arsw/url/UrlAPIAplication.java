513ae74a109e20d531de00553f903960816b075f
Edyesid <edyesid2000@gmail.com>
2020-11-20 17:26:47 -0500

    heroku

--- src/main/java/edu/arsw/url/UrlAPIAplication.java
+++ src/main/java/edu/arsw/url/UrlAPIAplication.java
@@ -1,10 +1,14 @@
 package edu.arsw.url;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+
 /**
  * @author Edwin Yesid Rodriguez Maldonado
  */
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 public class UrlAPIAplication {
     public static void main(String[] args) {
         SpringApplication.run(UrlAPIAplication.class, args);
