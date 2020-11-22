f92b375d98b7d95233992803912a2bbdc1f8182e
Edyesid <edyesid2000@gmail.com>
2020-11-21 19:27:50 -0500

    heroku conf

--- src/main/java/edu/arsw/url/persistence/MongoConfig.java
+++ src/main/java/edu/arsw/url/persistence/MongoConfig.java
@@ -1,4 +1,4 @@
-/*package edu.arsw.url.persistence;
+package edu.arsw.url.persistence;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import org.springframework.beans.factory.annotation.Value;
@@ -7,18 +7,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 @Configuration
 public class MongoConfig {
-
     @Value("${spring.data.mongodb.uri}")
     private String uri;
     @Value("${spring.data.mongodb.database}")
     private String dbname;
     @Bean
     public MongoClient mongoClient() {
-        return MongoClients.create("mongodb+srv://edwin_arsw:edwin123@cluster0.s2ygl.mongodb.net/UrlShortingDB?retryWrites=true&w=majority");
+        return MongoClients.create(uri);
     }
     @Bean
     public MongoTemplate mongoTemplate() {
-        return new MongoTemplate(mongoClient(), "UrlShortingDB");
+        return new MongoTemplate(mongoClient(), dbname);
     }
 }
-*/
